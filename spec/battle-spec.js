174058594c27627aa5fd67d3000ce62d18215d34
Mike Lofgren <mikeylofgren@gmail.com>
2019-06-28 15:53:06 -0700

    add test for mercury.

--- spec/battle-spec.js
+++ spec/battle-spec.js
@@ -3,20 +3,19 @@ import { SolarCalc } from './../src/solarSheet.js';
 
 describe('SolarCalc', function () {
 
-  it('should correctly determine the users input date', function () {
-    let radMan = new SolarCalc('1932');
-    radMan.dayMaker()
-    console.log(radMan)
-    expect(radMan.daysAlive).toEqual([new Date('1932')]);
+  it('should correctly determine the users age', function () {
+    const steveBrule = new SolarCalc('1984');
+    steveBrule.ageMaker()
+    console.log(steveBrule)
+    expect(steveBrule.earthAge).toEqual(35);
   });
 
-  // it('should correctly determine whether character levels up', function () {
-  //     let skateboardDude = new SolarCalc(3,4)
-  //     let level = skateboardDude.level
-  //     skateboardDude.levelUp(55)
-  //     expect(skateboardDude.level).toEqual(level + 1);
-  //
-  //
-  //   });
+  it('should correctly determine the users age for Mercury', function () {
+      const ronaldMcdonald = new SolarCalc('1995')
+      ronaldMcdonald.ageMaker()
+      ronaldMcdonald.mercuryAge()
+      console.log(ronaldMcdonald)
+      expect(ronaldMcdonald.mercury).toEqual(100);
 
-});
+    });
+  });
--- src/solarSheet.js
+++ src/solarSheet.js
@@ -13,24 +13,24 @@ export class SolarCalc {
     const yearBorn = getYear.getFullYear()// extracts Actual year born
     const currentDay = new Date //creates a new date
     const currentYear = currentDay.getFullYear()// gets actual year for new date
-    this.earthAge = currentYear - yearBorn //gets actual age
+    this.earthAge = currentYear - yearBorn - 1 //gets actual age
     }
 
   mercuryAge () {
     this.mercury = this.earthAge / .24
   }
 
-  venusAge () {
-    this.venus = this.earthAge / .62
-  }
-
-  marsAge () {
-    this.mars = this.earthAge / 1.88
-  }
-
-  jupiterAge () {
-    this.jupiter = this.earthAge / 11.86
-  }
+  // venusAge () {
+  //   this.venus = this.earthAge / .62
+  // }
+  //
+  // marsAge () {
+  //   this.mars = this.earthAge / 1.88
+  // }
+  //
+  // jupiterAge () {
+  //   this.jupiter = this.earthAge / 11.86
+  // }
 };
 
 let mike = new SolarCalc ('1984')
