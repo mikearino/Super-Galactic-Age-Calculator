80316bf4808e17485ad78c7299c5d1f7af286523
Mike Lofgren <mikeylofgren@gmail.com>
2019-06-28 15:28:45 -0700

    add methods for all planets.

--- src/solarSheet.js
+++ src/solarSheet.js
@@ -1,16 +1,36 @@
 export class SolarCalc {
   constructor (year){
     this.year = year,
-    this.earthAge = ''
+    this.earthAge = '',
+    this.mercury = '',
+    this.venus = '',
+    this.mars = '',
+    this.jupiter = ''
   }
 
-  dayMaker () {
+  ageMaker () {
     const getYear = new Date(this.year) // gets full year with junk
-    let yearBorn = getYear.getFullYear()// extracts Actual year born
+    const yearBorn = getYear.getFullYear()// extracts Actual year born
     const currentDay = new Date //creates a new date
     const currentYear = currentDay.getFullYear()// gets actual year for new date
-    this.earthAge = currentYear - yearBorn 
+    this.earthAge = currentYear - yearBorn //gets actual age
     }
+
+  mercuryAge () {
+    this.mercury = this.earthAge / .24
+  }
+
+  venusAge () {
+    this.venus = this.earthAge / .62
+  }
+
+  marsAge () {
+    this.mars = this.earthAge / 1.88
+  }
+
+  jupiterAge () {
+    this.jupiter = this.earthAge / 11.86
+  }
 };
 
 let mike = new SolarCalc ('1984')
