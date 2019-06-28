295af356229f24e30c0b87270b18c61e3bcb62ed
Mike Lofgren <mikeylofgren@gmail.com>
2019-06-28 12:19:31 -0700

    first spec fail, now pass.

--- spec/battle-spec.js
+++ spec/battle-spec.js
@@ -6,7 +6,7 @@ describe('SolarCalc', function () {
     let radMan = new SolarCalc('1932');
     radMan.dayMaker()
     console.log(radMan)
-    expect(radMan.dayMaker).toEqual(Thu Dec 31 1931 16:00:00 GMT-0800 (Pacific Standard Time));
+    expect(radMan.daysAlive).toEqual([new Date('1932')]);
   });
 
   // it('should correctly determine whether character levels up', function () {
