52a0d73234541550cd4faf74cf4a9ad7474c07e1
Mike Lofgren <mikeylofgren@gmail.com>
2019-06-28 08:44:16 -0700

    change naming conventions for files.

--- spec/battle-spec.js
+++ spec/battle-spec.js
@@ -1,21 +1,21 @@
-// import { Character } from './../src/battleMaster.js';
-//
-// describe('Character', function () {
-//
-//   it('should correctly determine whether a character has a new Id', function () {
-//     let radMan = new Character(3, 4);
-//     radMan.assignId()
-//     console.log(radMan.currentId)
-//     expect(radMan.currentId).toEqual(1);
-//   });
-//
-//   it('should correctly determine whether character levels up', function () {
-//       let skateboardDude = new Character(3,4)
-//       let level = skateboardDude.level
-//       skateboardDude.levelUp(55)
-//       expect(skateboardDude.level).toEqual(level + 1);
-//
-//
-//     });
-//
-// });
+import { SolarCalc } from './../src/solarSheet.js';
+
+describe('SolarCalc', function () {
+
+  it('should correctly determine whether a character has a new Id', function () {
+    let radMan = new SolarCalc(3, 4);
+    radMan.assignId()
+    console.log(radMan.currentId)
+    expect(radMan.currentId).toEqual(1);
+  });
+
+  it('should correctly determine whether character levels up', function () {
+      let skateboardDude = new SolarCalc(3,4)
+      let level = skateboardDude.level
+      skateboardDude.levelUp(55)
+      expect(skateboardDude.level).toEqual(level + 1);
+
+
+    });
+
+});
