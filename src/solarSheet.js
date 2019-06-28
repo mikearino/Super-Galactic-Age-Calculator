21e6b066fd25bac9c016c254102a5959acb2f66e
Mike Lofgren <mikeylofgren@gmail.com>
2019-06-28 16:31:10 -0700

    clean up code.

--- src/solarSheet.js
+++ src/solarSheet.js
@@ -27,32 +27,10 @@ export class SolarCalc {
   marsAge () {
     this.mars = this.earthAge / 1.88
   }
-  
+
   jupiterAge () {
     this.jupiter = this.earthAge / 11.86
   }
 };
 
 let mike = new SolarCalc ('1984')
-//   assignId () {
-//     this.currentId += 1;
-//     return this.currentId;
-//   }
-//   levelUp (exp) {
-//     this.exp += exp
-//     if (this.exp >= 50) {
-//       this.level++
-//     }
-//   };
-
-
-
-  // battle (enemy) {
-  //   char2.hp -= this.attack
-  //   if (char2.hp <= 0) {
-  //     char2.life = false;
-  //   }
-  //   newAttack (foundAttack){
-  //     char1
-  //   }
-  // }
